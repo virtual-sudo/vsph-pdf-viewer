95f6a59cded9872708218e5680efc93fda757bcd
lance757 <lance@virtualstudios.ph>
2026-08-27 15:26:22 +0800

    Fix Netlify deployment for pdfjs by renaming build to lib

--- external/pdfjs/web/viewer.js
+++ external/pdfjs/web/viewer.js
@@ -4209,7 +4209,7 @@ var pdfjsLib;
 if (typeof window !== 'undefined' && window['pdfjs-dist/build/pdf']) {
   pdfjsLib = window['pdfjs-dist/build/pdf'];
 } else {
-  pdfjsLib = require('../build/pdf.js');
+  pdfjsLib = require('../lib/pdf.js');
 }
 
 module.exports = pdfjsLib;
@@ -5272,7 +5272,7 @@ var defaultOptions = {
     kind: OptionKind.WORKER
   },
   workerSrc: {
-    value: '../build/pdf.worker.js',
+    value: '../lib/pdf.worker.js',
     kind: OptionKind.WORKER
   }
 };
